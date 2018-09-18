654b73fdd2887b1440a3c824882b23a746bea81d
sibhat <sibhat.temelso@gmail.com>
2018-09-18 14:42:03 -0700

    model added

--- hello_world_app/models.py
+++ hello_world_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from uuid import uuid4
 # Create your models here.
+class Graph(models.Model):
+    # id= models.UUID()
+    title= models.CharField(max_length=200)
+    content=models.TextField(blank=True)
+
