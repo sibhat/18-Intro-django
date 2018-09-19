bd3f388ecc148b2e8908d97060a80afe2a63ab2e
sibhat <sibhat.temelso@gmail.com>
2018-09-18 17:04:53 -0700

    database added to the app, and the app added to the project

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Graph'
+    'hello_world_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
