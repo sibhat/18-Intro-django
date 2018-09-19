7c2ee45fafe19767d5f01cc4c8c7f3e67b86e840
sibhat <sibhat.temelso@gmail.com>
2018-09-19 16:32:39 -0700

    added tag and author field to Graph table, and to the admin

--- hello_world_app/admin.py
+++ hello_world_app/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Graph
 # Register your models here.
-admin.site.register(Graph)
+
+class GraphAdmin(admin.ModelAdmin):
+# To get the read-only fields to show up in the interface: 
+    readonly_fields = ('created_at', 'last_modified')
+
+admin.site.register(Graph, GraphAdmin)
--- hello_world_app/models.py
+++ hello_world_app/models.py
@@ -5,4 +5,14 @@ class Graph(models.Model):
     id= models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title= models.CharField(max_length=200)
     content=models.TextField(blank=True)
+    tags = models.TextField(blank=True)
+    author=models.TextField(
+        max_length=100, default='no author'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    # auto_now_add only set date at creatation
+    last_modified = models.DateTimeField(auto_now=True)
+    # autp_now set time at create and update
 
+    def __repr__(self):
+        return f"{title}"
